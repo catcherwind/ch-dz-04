66a00b00d5b90d0c1b2f0e34ea1b2ea09a16ff23
Ksusha <ksushalive@yandex.ru>
2022-12-01 20:13:16 +0300

    init: solution of task 25

---
+++ z25/Program.cs
@@ -0,0 +1,13 @@
+﻿int Degree(int a, int b){
+    int d = a;
+    for (int i = 1; i < b; i++){
+        d *= a;
+    }
+    return d;
+}
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Degree(a,b));
