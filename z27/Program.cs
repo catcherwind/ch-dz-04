7f3b56de6f9899b1603e7a1c1a090cf095650d05
Ksusha <ksushalive@yandex.ru>
2022-12-01 20:25:36 +0300

    init: solution of task 27

---
+++ z27/Program.cs
@@ -0,0 +1,12 @@
+﻿int Summ(int n){
+    int sum = 0;
+    while (n != 0){
+        sum += n%10;
+        n /= 10;
+    }
+    return Math.Abs(sum);
+}
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Summ(n));
